964406a4a73a49d132841be7d0e2ea24841bc888
pangpond <pondzz@gmail.com>
2024-05-29 17:41:34 +0700

    add content for test rebase

--- midend/views/site/index.php
+++ midend/views/site/index.php
@@ -8,6 +8,7 @@ $this->title = 'My Yii Application';
     <div class="p-5 mb-4 bg-transparent rounded-3">
         <div class="container-fluid py-5 text-center">
             <h1 class="display-4">Congratulations! Magic!</h1>
+            <h2>Here is for rebase</h2>
             <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
             <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
         </div>
